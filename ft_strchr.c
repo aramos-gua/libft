a81fdc86928489a0d3170d65624aa4cea1bb540f
Alejandro Ramos <aramos@ramos.42london.com>
2024-12-01 19:36:31 +0000

    added tolower and strchr

---
+++ ft_strchr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramos <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 17:42:37 by aramos            #+#    #+#             */
+/*   Updated: 2024/12/01 19:33:01 by aramos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+#include <stddef.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (s == NULL || c < 0)
+		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;	
+	}
+	if (*s == '\0' && c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+//
+//int	main(void)
+//{
+//	char	s[] = "\0abcdef";
+//
+//	printf("%s", ft_strchr(s, 0));
+//	return (0);
+//}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aramos <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 17:29:26 by aramos            #+#    #+#             */
-/*   Updated: 2024/12/01 17:34:04 by aramos           ###   ########.fr       */
+/*   Updated: 2024/12/01 18:01:26 by aramos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,5 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
