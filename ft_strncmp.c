2661ca8c06c0a499a7cb1d8249c7e9df3747e68e
Alex <alejandro.ramos.gua@gmail.com>
2024-12-08 14:07:59 +0000

    fixed strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aramos <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 17:56:49 by aramos            #+#    #+#             */
-/*   Updated: 2024/12/07 14:59:14 by aramos           ###   ########.fr       */
+/*   Updated: 2024/12/08 14:04:55 by aramos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,30 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t				i;
+	unsigned const char	*first;
+	unsigned const char	*second;
 
 	i = 0;
+	first = (unsigned const char *) s1;
+	second = (unsigned const char *) s2;
 	if (n == 0)
 		return (0);
-	while (i < (n - 1) && s1[i] != '\0' && s2[i] != '\0')
+	while (i < (n - 1) && first[i] != '\0' && second[i] != '\0')
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (first[i] != second[i])
+			return (first[i] - second[i]);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (first[i] - second[i]);
 }
 //
 //int	main(void)
 //{
-//	const char	s1[] = "1234";
-//	const char	s2[] = "1235";
+//	//const char	s1[] = "1234";
+//	//const char	s2[] = "1235";
 //
-//	printf("Mine: %d\n", ft_strncmp(s1, s2, 3));
-//	printf("Original: %d", strncmp(s1, s2, 3));
+//	printf("Mine: %d\n", ft_strncmp("test\200", "test\0", 6));
+//	printf("Original: %d", strncmp("test\200", "test\0", 6));
 //	return (0);
 //}
