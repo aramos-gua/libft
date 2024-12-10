9bccbaef1c6ff87bd4892532a4f57e0385a933e9
Alex <alejandro.ramos.gua@gmail.com>
2024-12-10 09:09:11 +0000

    finished strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,8 +6,38 @@
 /*   By: aramos <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 19:15:27 by aramos            #+#    #+#             */
-/*   Updated: 2024/12/09 19:15:29 by aramos           ###   ########.fr       */
+/*   Updated: 2024/12/10 09:03:16 by aramos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include "libft.h"
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s3;
+	char	*ptr_3;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	s3 = (char *) malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (s3 == NULL)
+		return (NULL);
+	s3[0] = '\0';
+	ptr_3 = s3;
+	ft_strlcat(s3, s1, (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	ft_strlcat(s3, s2, (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	return (ptr_3);
+}
+//
+//int	main (void)
+//{
+//	char	*s1;
+//	char	*s2;
+//
+//	s1 = "";
+//	s2 = " como estas?";
+//	printf("Mine: %s", ft_strjoin(s1, s2));
+//	return (0);
+//}
+//
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aramos <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 17:29:26 by aramos            #+#    #+#             */
-/*   Updated: 2024/12/07 15:04:15 by aramos           ###   ########.fr       */
+/*   Updated: 2024/12/10 07:49:05 by aramos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,3 +37,4 @@ int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
